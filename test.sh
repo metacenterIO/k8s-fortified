955a4213ed55f5134da1dbd947132b9e5a0e6392
Concourse Automation <michael.ward@sapns2.com>
2021-08-04 08:08:31 -0600

    remove test file

--- test.sh
+++
@@ -1,5 +0,0 @@
-kubectl get pods \
---all-namespaces \
---field-selector metadata.namespace!=kube-system,metadata.namespace!=kube-public,metadata.namespace!=kube-node-lease \
--o json \
-| jq -e --arg hp 1024 -r '[.items[] | .spec.containers[].ports[]?.hostPort // empty] | select( all( . < $hp)) | length > 0' && exit 1 || exit 0
